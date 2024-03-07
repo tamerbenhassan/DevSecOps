a80b393d64ed2a01548d9260b9f6d76a7c59c8c5
Tamer Ben <137995765+tamerbenhassan@users.noreply.github.com>
2024-03-07 13:25:29 +0100

    Update k8s-deployment.sh

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -1,5 +1,5 @@
 sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
-kubectl -n default get deployment ${deploymentName} > /dev/null
+# kubectl -n default get deployment ${deploymentName} > /dev/null
 
 # if [[ $? -ne 0 ]]; then
 #     echo "deployment ${deploymentName} doesnt exist"
