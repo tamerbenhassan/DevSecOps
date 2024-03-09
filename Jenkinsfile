8bfb80014b33b6dfd98821572ff9bd6e73f91b8b
Tamer Ben <137995765+tamerbenhassan@users.noreply.github.com>
2024-03-09 18:17:27 +0100

    Added OWASP ZAP scan
    - Added stage to jenkinsfile
    - Added Dependency in pom.xml
    - Created zap.sh

--- Jenkinsfile
+++ Jenkinsfile
@@ -119,6 +119,14 @@ pipeline {
         }
       }
 
+      stage('OWASP ZAP - DAST') {
+        steps {
+          withKubeConfig([credentialsId: 'kubeconfig']) {
+            sh "bash zap.sh"
+          }
+        }
+      }
+
     }
 
     post {
---
+++ zap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
+
+# first run this
+chmod 777 $(pwd)
+echo $(id -u):$(id -g)
+docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
+exit_code=$?
+
+# HTML report
+  sudo mkdir -p owasp-zap-report
+  sudo mv zap_report.html owasp-zap-report 
+
+echo "Exit Code : $exit_code"
+
+  if [[ ${exit_code} -ne 0 ]]; then
+    echo "OWASP ZAP report has either Low/Meduim/High Risk. Please check the HTML Report"
+    exit 1;
+  else
+    echo "OWASP ZAP did not report any Risk"
+  fi; 
